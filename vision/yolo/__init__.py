9c9750d1ed02ef6d43d89008cd4badf7b33f2bf0
Juan Casal <juan.5ht@gmail.com>
2026-09-11 23:25:43 -0300

    Expose explainability ML and video IO helpers

--- vision/yolo/__init__.py
+++ vision/yolo/__init__.py
@@ -1,8 +1,10 @@
 """vision.yolo – modular YOLO wrapper for scientific pipelines."""
 
 from vision.yolo.devices import detect_device, get_device_info
+from vision.yolo.explain import detection_density_map, occlusion_sensitivity, plot_density_map
 from vision.yolo.export import export_model
 from vision.yolo.infer import predict_directory, predict_image, predict_images, predict_video
+from vision.yolo.ml import frame_features, merge_labels, prepare_xy, shap_values, track_features
 from vision.yolo.track import make_tracker_config, track_detections_dataframe, track_video
 from vision.yolo.train import resume_training, train_model, validate_model
 from vision.yolo.video import (
@@ -10,6 +12,7 @@ from vision.yolo.video import (
     write_annotated_video,
     write_annotated_video_from_dataframe,
 )
+from vision.yolo.video_io import extract_frames, images_to_video, read_frames, video_info, write_frames
 
 __all__ = [
     "detect_device",
@@ -24,6 +27,19 @@ __all__ = [
     "draw_predictions_on_frame",
     "write_annotated_video",
     "write_annotated_video_from_dataframe",
+    "read_frames",
+    "write_frames",
+    "extract_frames",
+    "images_to_video",
+    "video_info",
+    "detection_density_map",
+    "plot_density_map",
+    "occlusion_sensitivity",
+    "frame_features",
+    "track_features",
+    "merge_labels",
+    "prepare_xy",
+    "shap_values",
     "train_model",
     "resume_training",
     "validate_model",
